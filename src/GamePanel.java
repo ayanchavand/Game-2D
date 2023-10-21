1cc558f12d1bfcfda62d73922af602e32148734a
ayanchavand <ayan.chavand15@gmail.com>
2023-10-21 15:35:41 +0530

    Implemented GameThread

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,7 +1,7 @@
 import javax.swing.JPanel;
 import java.awt.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable{
 
     //RENDER SETTINGS
     final int originalTileSize =  20;
@@ -12,6 +12,8 @@ public class GamePanel extends JPanel {
     final int screenWidth = maxScreenCol * tileSize;
     final int screenHeight = maxScreenRow * tileSize;
 
+    Thread gameThread;
+
     //GamePanel constructor for applying the window settings
     public GamePanel(){
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
@@ -19,4 +21,16 @@ public class GamePanel extends JPanel {
         this.setDoubleBuffered(true);
     }
 
+    public void startGameThread(){
+        gameThread = new Thread(this);
+       gameThread.start();
+    }
+    @Override
+    public void run() {
+        while (gameThread != null) {
+            System.out.println("Game Thread is active! LESGOO");
+            //1. Update
+            //2. DrawCall
+        }
+    }
 }
